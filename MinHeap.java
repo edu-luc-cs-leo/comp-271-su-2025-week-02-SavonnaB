7e8f48d7d15c2bde1fb8ae8ec23c46d653bcfd20
SavonnaB <143576792+SavonnaB@users.noreply.github.com>
2025-06-11 20:28:36 +0000

    terminal states line 217 has illegal start

--- MinHeap.java
+++ MinHeap.java
@@ -214,7 +214,7 @@ public class MinHeap {
      * method takes at most log(n) steps where n=this.usage, i.e., the number of
      * used elements in the underlying array.
      */
-    private void heapifyDown() {
+    private void heapifyDown(){
         // Starting point
         int parent = 0;
         // Indices for left and right child and the smallest of them.
